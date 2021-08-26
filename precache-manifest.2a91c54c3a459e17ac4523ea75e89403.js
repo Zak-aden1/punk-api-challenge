8efb9240bccf69014a6ab15b249e2d4eacb327ce
zak aden <faf>
2021-08-26 14:13:06 +0100

    Updates

---
+++ precache-manifest.2a91c54c3a459e17ac4523ea75e89403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dd5d878cb46b60293e1579f4a3ffc3",
+    "url": "/Zak-aden1/punk-api-challenge/index.html"
+  },
+  {
+    "revision": "c64423e6b1f5217f7141",
+    "url": "/Zak-aden1/punk-api-challenge/static/css/main.6af211f0.chunk.css"
+  },
+  {
+    "revision": "393c685f09943f72e642",
+    "url": "/Zak-aden1/punk-api-challenge/static/js/2.c54cf1a7.chunk.js"
+  },
+  {
+    "revision": "36df01a1ea974372bd582b9d7211a911",
+    "url": "/Zak-aden1/punk-api-challenge/static/js/2.c54cf1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64423e6b1f5217f7141",
+    "url": "/Zak-aden1/punk-api-challenge/static/js/main.87b18e93.chunk.js"
+  },
+  {
+    "revision": "aef2468432d9961e68a5",
+    "url": "/Zak-aden1/punk-api-challenge/static/js/runtime-main.5fb52b57.js"
+  }
+]);
